7adb06e71f6ddc518c04c8b63b9d52d2935f129f
Michael Stevan <miestgo@gmail.com>
2018-05-09 19:39:07 +0200

    Cleaned up code and graphs. Now I'm gonna make use of plotly.

--- Assignment2-Bertin-ggplot.R
+++ Assignment2-Bertin-ggplot.R
@@ -2,42 +2,25 @@ library(ggplot2)
 library(ggthemes)
 library(data.table)
 library(dplyr)
-library(corrplot)
 
+#########################
 # Read movie data
+#########################
 movies <- fread('https://raw.githubusercontent.com/idc9/stor390/master/data/movies.csv')
 head(movies)
 colnames(movies)
 
-unique(movies$genre)
-
+#########################
 # Plotting graphs
-
-# Default graph
-ggplot(data = movies) + 
-  geom_point(mapping = aes(x=imdb_num_votes, y=imdb_rating, shape=genre, color=best_dir_win))+
-  scale_x_continuous()+
-  scale_y_continuous()+
-  theme_light() +
-  theme(legend.position = "right", legend.key = element_blank(),
-        axis.text = element_text(size=10),
-        axis.title = element_text(size=12)) +
-  labs(x = "Label x-axis", y = "Label y-axis", title="Title of your visualization", color="Label colors", shape = "Label shapes")
-ggsave("assignment2-ggplot-movies.png", width = 8, height = 8)
-
-
-# Plot 1
-
-# moviesQuantitative <- movies %>% mutate(best_actor_win = ifelse((best_actor_win=="no"),0,1),best_actress_win = ifelse((best_actress_win=="no"),0,1))%>% select(imdb_rating,critics_score,best_actress_win,best_actor_win)
-# M <- cor(moviesQuantitative)
-# # corrplot.mixed(cor(moviesQuantitative),method="circle",order="AOE")
-# corrplot.mixed(cor(moviesQuantitative),order="AOE")
-
-movies %>% count(genre) %>% arrange(desc(n)) %>% head(n=5)
+#########################
 
 # Most popular genres
+movies %>% count(genre) %>% arrange(desc(n)) %>% head(n=5)
+
+# Keep main genres only
 movies3MainGenres <- movies %>% filter(genre=="Drama" | genre=="Comedy" | genre=="Action & Adventure")
 
+# Tidy up data
 movies3MainGenres$critics_rating<- gsub(movies3MainGenres$critics_rating,pattern = "Certified Fresh",replacement="Very Good")
 movies3MainGenres$critics_rating <- gsub(movies3MainGenres$critics_rating,pattern = "Fresh",replacement="Good")
 movies3MainGenres$critics_rating <- gsub(movies3MainGenres$critics_rating,pattern = "Rotten",replacement="Poor")
@@ -49,27 +32,30 @@ ggplot(data = movies3MainGenres) +
   geom_jitter(mapping = aes(x=audience_score, y=imdb_rating, color = genre,  size = imdb_num_votes, shape = critics_rating),alpha = 0.7)+
   scale_x_continuous()+
   scale_y_continuous()+
+  scale_size(range = c(2, 7))+
   theme_light() +
   theme(legend.position = "right", legend.key = element_blank(),
         axis.text = element_text(size=10),
         axis.title = element_text(size=12)) +
-  labs(x = "Rotten tomatoes - Score", y = "IMDB-Score", title="IMDB vs Rotten Tomatoes Rating", color="Genres", shape = "Review Quality (Rotten Tomatoes)", size  = "Number of imdb votes")
+  labs(x = "Rotten Tomatoes - Score", y = "IMDB-Score", title="IMDB vs Rotten Tomatoes Rating", color="Genres", shape = "Quality of Reviews (Rotten Tomatoes)", size  = "Number of IMDB votes")
+
+ggsave("plot1.png", width = 12, height = 8)
 
 
-# Plot Comparing time vs avg_score, Genres, Number of Imdv votes and Review Quality.
-movies3MainGenres2 <- 
-  
+# Dates in X axis, Avg Audience & Critic score & IMDB (Normalize) in Y axis, 4 Main Genres hue, 
+movies3MainGenres$critics_score <- movies3MainGenres$critics_score/10
+movies3MainGenres$audience_score <- movies3MainGenres$audience_score/10
+movies3MainGenres$AVG <- (movies3MainGenres$audience_score + movies3MainGenres$critics_score + movies3MainGenres$imdb_rating)/3
+movies3MainGenres$AVG <- as.numeric(movies3MainGenres$AVG) 
+
 ggplot(data = movies3MainGenres) + 
-  geom_jitter(mapping = aes(x=critics_score, y=imdb_rating, color = genre,  size = imdb_num_votes, shape = critics_rating),alpha = 0.7)+
+  geom_jitter(mapping = aes(x=thtr_rel_year, y=AVG, color = genre,  size = imdb_num_votes, shape = critics_rating),alpha = 0.7)+
   scale_x_continuous()+
   scale_y_continuous()+
   theme_light() +
   theme(legend.position = "right", legend.key = element_blank(),
         axis.text = element_text(size=10),
         axis.title = element_text(size=12)) +
-  labs(x = "Critics score, Rotten tomatoes", y = "Imdb-score", title="Imdb vs Rotten tomatoes", color="Genres", shape = "Review Quality (Rotten Tomatoes)", size  = "Number of imdb votes")
-
-
-# Dates in X axis, Avg Audience & Critic score & IMDB (Normalize) in Y axis, 4 Main Genres hue, 
+  labs(x = "Rotten tomatoes - Score", y = "IMDB-Score", title="Avg Score Vs Time", color="Genres", shape = "Review Quality (Rotten Tomatoes)", size  = "Number of imdb votes")
 
 
